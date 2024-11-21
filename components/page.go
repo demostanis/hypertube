6df6b64c3deb621127d108604442b442594211e6
demostanis <demostanis@protonmail.com>
2024-11-21 10:21:04 +0100

    allow page to take multiple components

--- components/page.go
+++ components/page.go
@@ -8,7 +8,7 @@ import (
 
 var bulma = "https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
 
-func PageWithTitle(content Node, title string) Node {
+func PageWithTitle(title string, content ...Node) Node {
 	return HTML5(HTML5Props{
 		Title: title,
 		Head: []Node{
@@ -17,10 +17,10 @@ func PageWithTitle(content Node, title string) Node {
 				Href(bulma),
 			),
 		},
-		Body: []Node{content},
+		Body: content,
 	})
 }
 
-func Page(content Node) Node {
-	return PageWithTitle(content, "Hypertube")
+func Page(content ...Node) Node {
+	return PageWithTitle("Hypertube", content...)
 }
