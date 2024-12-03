d256f1cbb49af14dadade87f971ad655b37551bf
acasamit <165587864+acasamit@users.noreply.github.com>
2024-12-03 21:53:46 +0000

    scroll system not making call to server anymore

--- app/main.go
+++ app/main.go
@@ -16,8 +16,6 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/handle-scroll-left", handleScrollLeft)
-	http.HandleFunc("/handle-scroll-right", handleScrollRight)
 
 	r := pat.New()
 
