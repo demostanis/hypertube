21f1e80bf76cea5903684089e9d1a922630cd7e8
acasamit <165587864+acasamit@users.noreply.github.com>
2024-12-06 20:58:27 +0100

    Reworked navbar and over on film (very temporary code)

--- app/pages/home.go
+++ app/pages/home.go
@@ -4,8 +4,9 @@ import (
 	"github.com/demostanis/hypertube/components"
 
 	. "maragu.dev/gomponents"
+	. "maragu.dev/gomponents/html"
 )
 
 func Home() Node {
-	return components.Page(components.Navbar(), components.CardGrill(), components.Foot())
+	return components.Page(Class("has-navbar-fixed-top"), components.Navbar(), components.CardGrill(), components.Foot())
 }
