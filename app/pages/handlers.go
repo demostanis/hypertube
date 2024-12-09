c774e6abd5cc00c05a4d1e3005676196e54d073d
acasamit <165587864+acasamit@users.noreply.github.com>
2024-12-09 22:55:16 +0100

    scroll system not making call to server anymore

--- app/pages/handlers.go
+++ app/pages/handlers.go
@@ -1,42 +1,10 @@
 package pages
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
 
 	. "maragu.dev/gomponents"
 )
-
-func handleScrollLeft(w http.ResponseWriter, r *http.Request) {
-	scrollLeft := r.URL.Query().Get("scrollLeft")
-	offsetWidth := r.URL.Query().Get("offsetWidth")
-
-	scrollLeftInt, _ := strconv.Atoi(scrollLeft)
-	offsetWidthInt, _ := strconv.Atoi(offsetWidth)
-
-	if scrollLeftInt-offsetWidthInt <= 0 {
-		fmt.Fprintf(w, "0")
-	} else {
-		fmt.Fprintf(w, "1")
-	}
-}
-
-func handleScrollRight(w http.ResponseWriter, r *http.Request) {
-	scrollLeft := r.URL.Query().Get("scrollLeft")
-	offsetWidth := r.URL.Query().Get("offsetWidth")
-	maxScroll := r.URL.Query().Get("maxScroll")
-
-	scrollLeftInt, _ := strconv.Atoi(scrollLeft)
-	offsetWidthInt, _ := strconv.Atoi(offsetWidth)
-	maxScrollInt, _ := strconv.Atoi(maxScroll)
-
-	if scrollLeftInt+offsetWidthInt >= maxScrollInt {
-		fmt.Fprintf(w, "0")
-	} else {
-		fmt.Fprintf(w, "1")
-	}
-}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) (Node, error) {
 	return Home(), nil
