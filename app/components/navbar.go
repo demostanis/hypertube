5ad0f49b6c659db4fbffb8d4f35a5fe37d394ade
acasamit <165587864+acasamit@users.noreply.github.com>
2024-12-09 22:55:56 +0100

    make navbar fully responsive

--- app/components/navbar.go
+++ app/components/navbar.go
@@ -31,10 +31,21 @@ func navbarItemEnd(name, path, icon string) Node {
 	)
 }
 
+func navbarItemMobil(pos, path, icon string) Node {
+	return A(
+		Attr("style", "position: absolute; color: mediumslateblue; top: 25%; left: "+pos+";"),
+		Href(path),
+		Span(
+			Class("icon"),
+			I(Class("fa-solid fa-"+icon)),
+		),
+	)
+}
+
 func Navbar() Node {
 	return Nav(Class("navbar is-fixed-top"),
 		Attr("style", "background-color: #1f2226;"),
-		Div(Class("navbar-menu"),
+		Div(Class("navbar-menu is-hidden-mobile"),
 			Div(Class("navbar-start"),
 				Attr("style", "margin-left: 68px"),
 				navbarItemStart("Home", "/", "house"),
@@ -45,6 +56,12 @@ func Navbar() Node {
 				navbarItemEnd("Research...", "/", "magnifying-glass"),
 				navbarItemEnd("Login", "/login", "user"),
 			),
+		),
+		Div(Class("navbar-menu is-hidden-tablet"),
+			navbarItemMobil("15vw", "/", "house"),
+			navbarItemMobil("35vw", "/videos", "film"),
+			navbarItemMobil("60vw", "/", "magnifying-glass"),
+			navbarItemMobil("80vw", "/login", "user"),
 		),
 	)
 }
