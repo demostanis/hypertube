162f699a0bd7e24c16ff30b26ce545232488f0ce
acasamit <165587864+acasamit@users.noreply.github.com>
2024-12-09 22:55:54 +0100

    make content popup fully responsive

--- app/components/popup.go
+++ app/components/popup.go
@@ -6,11 +6,23 @@ import (
 )
 
 func CreatePopupHeader(ContentTitle string) Node {
+	ContentTitleMobile := ContentTitle
+	if len(ContentTitle) >= 40 {
+		ContentTitle = ContentTitle[:40] + "..."
+	}
+	if len(ContentTitleMobile) >= 24 {
+		ContentTitleMobile = ContentTitleMobile[:24] + "..."
+	}
 	return Header(Class("modal-card-head"),
 		P(
-			Class("modal-card-title"),
+			Class("modal-card-title is-hidden-mobile"),
 			Attr("style", "text-align: center; padding-left: 20px;"),
 			Text(ContentTitle),
+		),
+		P(
+			Class("modal-card-title is-hidden-tablet"),
+			Attr("style", "text-align: center; padding-left: 20px;"),
+			Text(ContentTitleMobile),
 		),
 		Button(Class("delete"),
 			Attr("hx-get", "/empty"),
@@ -25,34 +37,45 @@ func CreatePopupTrailer(TrailerLink, ContentImage string) Node {
 	Style := "background-color: rgba(20, 22, 26, 1); padding: 15px; padding-bottom: 0px;"
 
 	if TrailerLink != "" {
-		return IFrame(
-			ID("trailer"),
-			Src(TrailerLink),
-			Attr("allowfullscreen"),
-			Attr("frameborder", "0"),
-			Attr("width", "640"),
-			Attr("height", "360"),
-			Attr("style", Style),
+		return Div(
+			Attr("style", "background-color: rgba(20, 22, 26, 1);"),
+			IFrame(
+				Class("is-hidden-mobile"),
+				Src(TrailerLink),
+				Attr("allowfullscreen"),
+				Attr("frameborder", "0"),
+				Attr("width", "640"),
+				Attr("height", "360"),
+				Attr("style", Style),
+			),
+			IFrame(
+				Class("is-hidden-tablet"),
+				Src(TrailerLink),
+				Attr("allowfullscreen"),
+				Attr("frameborder", "0"),
+				Attr("style", "width: 100vw; height: calc(100vw / 1.77777777777);"),
+			),
 		)
 	}
 	if ContentImage != "" {
 		return Img(
+			Class("image is-16by9"),
 			Src("https://image.tmdb.org/t/p/original"+ContentImage),
-			Attr("style", Style+"width: 640px; height: 360px;"),
+			Attr("style", Style),
 		)
 	}
 	return Div(
 		Text("No trailer was found about this content :("),
 		Br(),
 		Text("But don't worry! You can still watch it!"),
-		Attr("style", Style+"width: 640px; height: 360; text-align:center"),
+		Attr("style", Style+"text-align: center;"),
 	)
 }
 
 func CreatePopupFooter() Node {
 	return Footer(Class("modal-card-foot"),
 		Div(Class("buttons"),
-			Button(Class("button"), Attr("style", "outile: solid 1px mediumslateblue"),
+			Button(Class("button"), Attr("style", "outline: solid 1px mediumslateblue"),
 				Span(Attr("style", "color: mediumslateblue;"), Text("PLAY")),
 				Span(Class("icon"), Attr("style", "color: mediumslateblue;"),
 					I(Class("fa-solid fa-play"), Attr("aria-hidden", "true")),
