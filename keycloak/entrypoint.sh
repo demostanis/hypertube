d85dd70c3be99b205fed37c664600307b86d3b84
DinoMalin <jcario@student.42lehavre.fr>
2024-12-10 14:39:36 +0100

    add forward auth for jackett

--- keycloak/entrypoint.sh
+++ keycloak/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+rm -f /opt/keycloak/ready
+
 bin/kc.sh "$@" &
 
 ok() {
-	( exec 3<>/dev/tcp/localhost/8080
+	( exec 3<>/dev/tcp/localhost/8000
 	(
 	# imagine having your /health/ready brokenfajsdlfjk
 		echo GET /admin/master/console/ HTTP/1.1
@@ -20,7 +22,7 @@ done
 conf=$(mktemp)
 bin/kcadm.sh config credentials \
 	--config "$conf" \
-	--server http://localhost:8080 \
+	--server http://localhost:8000 \
 	--realm master \
 	--user "$KC_BOOTSTRAP_ADMIN_USERNAME" \
 	--password "$KC_BOOTSTRAP_ADMIN_PASSWORD" 
@@ -36,5 +38,31 @@ kcadm create clients -r default \
 	-s 'redirectUris=["http://localhost:8000"]' \
 	-s publicClient=true \
 	-s directAccessGrantsEnabled=true 
+
+FORWARD_AUTH_ID=$(kcadm create clients -r master \
+	-s clientId='forward-auth' \
+	-s 'redirectUris=["http://jackett.localhost:8000/oauth2/callback"]' \
+	-s publicClient=false \
+	-s "secret=$KC_FORWARD_AUTH_SECRET" --id)
+
+
+ADMIN_ID=$(kcadm get users -r master -q exact=true -q username=$KEYCLOAK_ADMIN | grep id | sed -e 's/"id" : "//' -e 's/",//' | xargs)
+
+kcadm update "users/$ADMIN_ID" \
+	-s emailVerified=true \
+	-s email=$KEYCLOAK_ADMIN_EMAIL
+
+
+if [ -n "$FORWARD_AUTH_ID" ]; then
+	kcadm create "clients/$FORWARD_AUTH_ID/protocol-mappers/models" -r master \
+		-s name=aud-mapper-forward-auth \
+		-s protocol=openid-connect \
+		-s protocolMapper=oidc-audience-mapper \
+		-s 'config."included.client.audience"=forward-auth' \
+		-s 'config."id.token.claim"=true' \
+		-s 'config."access.token.claim"=true'
+fi
+
+touch /opt/keycloak/ready
 
 wait
