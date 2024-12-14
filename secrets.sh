bcec4d2aa0f9577e8660d098d62ce66593f1c189
demostanis <demostanis@protonmail.com>
2024-12-15 00:08:33 +0100

    Makefile: generate automatically KEYCLOAK_FORWARD_AUTH_SECRET

--- secrets.sh
+++ secrets.sh
@@ -5,11 +5,12 @@ env_file=.env
 pwgen() {
 	pw=$(tr -dc '[:alnum:]' < /dev/urandom | head -c 32)
 
-	if ! grep -q "$1"_DB_PASSWORD= "$env_file"; then
-		echo "$1"_DB_PASSWORD="$pw" >> "$env_file"
+	if ! grep -q "$1"= "$env_file"; then
+		echo "$1"="$pw" >> "$env_file"
 	fi
 }
 
-pwgen HYPERTUBE
-pwgen KEYCLOAK
-pwgen ROOT
+pwgen HYPERTUBE_DB_PASSWORD
+pwgen KEYCLOAK_DB_PASSWORD
+pwgen ROOT_DB_PASSWORD
+pwgen KEYCLOAK_FORWARD_AUTH_SECRET
